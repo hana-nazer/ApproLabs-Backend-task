7cf5579780ade0112e6d40ec717b11df910f1724
hana-nazer <hanapp9966@gmail.com>
2023-05-03 22:01:23 +0530

    Login validation

--- controller/userController.js
+++ controller/userController.js
@@ -1,65 +1,82 @@
 const User = require("../model/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const validator = require('validator');
+const validator = require("validator");
 
-
+// Signup
 exports.postSignUp = async (req, res) => {
   try {
     // Check if all fields are provided
     const { username, email, password } = req.body;
     if (!username || !email || !password) {
-      return res.status(400).json({ error: 'All fields are required' });
+      return res.status(400).json({ error: "All fields are required" });
     }
 
     // Validate email
     if (!validator.isEmail(email)) {
-      return res.status(400).json({ error: 'Invalid email format' });
+      return res.status(400).json({ error: "Invalid email format" });
     }
 
     // Check if email already exists
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ error: 'Email already exists' });
+      return res.status(400).json({ error: "Email already exists" });
     }
 
     // Validate password
     if (!validator.isLength(password, { min: 3, max: 15 })) {
-      return res.status(400).json({ error: 'Password must be between 3 and 15 characters long' });
+      return res
+        .status(400)
+        .json({ error: "Password must be between 3 and 15 characters long" });
     }
 
     // Validate username
     if (!validator.matches(username, /^[a-zA-Z0-9_]+$/)) {
-      return res.status(400).json({ error: 'Username should not contain special characters except _' });
+      return res
+        .status(400)
+        .json({
+          error: "Username should not contain special characters except _",
+        });
     }
 
+    // hash the password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
     req.body.password = hashedPassword;
 
+    // saving the user
     const newUser = new User(req.body);
     await newUser.save();
-    res.json({ message: 'User created successfully' });
+    res.json({ message: "User created successfully" });
   } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 
-
-// login user
+// Login
 exports.postLogin = async (req, res) => {
   try {
+    // Check if both email and password fields are provided
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ error: "Both email and password are required" });
+    }
+
+    // Validate the email format
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({ error: "Invalid email format" });
+    }
+
     // check if user exists
-    const user = await User.findOne({ email: req.body.email });
+    const user = await User.findOne({ email });
     if (!user) {
       return res.status(401).json({ error: "User does not exist" });
     }
 
-    // check password valid or not
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
+    // check password
+    const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
       return res.status(401).json({ error: "Invalid password" });
     }
@@ -75,7 +92,6 @@ exports.postLogin = async (req, res) => {
   }
 };
 
-
 // get current user
 exports.getCurrentUser = async (req, res) => {
   try {
@@ -85,7 +101,6 @@ exports.getCurrentUser = async (req, res) => {
       return res.status(404).json({ error: "User not found" });
     }
 
-    // Send the user details as the response
     res.json({
       success: true,
       message: "User details fetched successfully",
